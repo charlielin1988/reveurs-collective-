a767320bae695afbc80836d67ef5ca3250b9277a
shelbylin13 <shelbylin13@gmail.com>
2022-02-07 14:42:15 -0600

    created model User and migrated

--- reveurs_backend/reveurs/models.py
+++ reveurs_backend/reveurs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    pronouns = models.CharField(max_length=100)
+    profile_pic = models.TextField()
+    email = models.CharField(max_length=100)
+    passwordDigest = models.CharField(max_length=100)
+
+
+def __str__(self):
+    return self.name
