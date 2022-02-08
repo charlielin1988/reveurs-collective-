1a1e90b6a7cca138b1f9b752c5dca33ef32d14d1
shelbylin13 <shelbylin13@gmail.com>
2022-02-07 19:49:44 -0600

    Exhibition model and migration

--- reveurs_backend/reveurs/models.py
+++ reveurs_backend/reveurs/models.py
@@ -13,3 +13,26 @@ class User(models.Model):
 
 def __str__(self):
     return self.name
+
+
+class Location(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='locations')
+    city = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+def __str__(self):
+    return self.city
+
+
+class Exhibition(models.Model):
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, related_name='exhibitions')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    picture = models.TextField()
+
+    def __str__(self):
+        return self.title
